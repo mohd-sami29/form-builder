7a1d4eef009e0b34629d2212eb3c5a45400c9dd0
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-02-24 11:27:25 +0530

    add FormBuilder component

---
+++ src/pages/FormBuilder.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addForm } from "../store/formSlice";
+import { useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import FieldSettings from "../components/FieldSettings";
+
+function FormBuilder() {
+  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const selectedForm = useSelector((state) => state.form.selectedForm);
+
+  const handleSave = async () => {
+    const newForm = { ...selectedForm, title };
+    console.log("title-:", title);
+
+    try {
+      // POST request to save the form in the JSON server
+      const response = await fetch("http://localhost:5000/forms", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newForm),
+      });
+
+      if (response.ok) {
+        const savedForm = await response.json();
+        dispatch(addForm(savedForm)); // Save in Redux store
+        navigate("/forms"); // Redirect to forms list
+      } else {
+        console.error("Failed to save the form");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="flex w-full h-screen bg-white p-2 rounded-sm">
+      <Sidebar />
+      <div className="flex-1 p-8 pt-4 ">
+        <p className="bg-[#d7e8ff] p-3 text-lg font-semibold">Form Title</p>
+        <input
+          type="text"
+          placeholder="Enter Form Title"
+          className="border rounded-sm p-2 w-full"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <div className="mt-4 border p-2 rounded-sm bg-gray-50 overflow-auto max-h-[65vh]">
+          {selectedForm.fields.length > 0 ? (
+            selectedForm.fields.map((field) => (
+              <div key={field.id} className="mb-2">
+                <label className="block p-2 font-semibold">{field.label}</label>
+                {field.type === "radio" ? (
+                  field.options.map((option, index) => (
+                    <div key={index} className="flex items-center mb-1 ml-2">
+                      <input
+                        type="radio"
+                        readonly
+                        name={field.label}
+                        className="mr-2"
+                      />
+                      <span>{option}</span>
+                    </div>
+                  ))
+                ) : (
+                  <input
+                    type={field.type}
+                    className="border p-2 w-full rounded-md"
+                    placeholder={field.placeholder}
+                    readOnly
+                  />
+                )}
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">No fields added yet.</p>
+          )}
+        </div>
+
+        <button
+          className="bg-[#a3caff] text-black hover:bg-[#67a6ff] hover:text-white px-4 py-2 mt-2 shadow-lg rounded-md"
+          onClick={handleSave}
+        >
+          Save Form
+        </button>
+      </div>
+      <FieldSettings />
+    </div>
+  );
+}
+
+export default FormBuilder;
