12b07fa27d39284d25b169e60300ab89ac721821
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-02-24 16:01:28 +0530

    add tailwind classes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,52 +19,52 @@ function Sidebar() {
   ];
 
   return (
-    <>
-      <div className="w-1/4 bg-gray-100 p-4 flex flex-col justify-between items-center">
-        <div className="w-full">
-          <h3 className="text-lg font-bold">Form Components</h3>
-          <ul>
-            {fieldTypes.map(({ type, label, icon }) => (
-              <li
-                key={type}
-                className={`flex items-center gap-2 border p-2 my-2 cursor-pointer hover:bg-[#d7e8ff] ${
-                  selectedField === type ? "bg-[#d7e8ff]" : ""
+    <div className="w-full md:w-1/4 bg-gray-100 p-4 rounded-sm flex flex-col justify-between items-center h-full fixed md:relative">
+      <div className="w-full">
+        <h3 className="text-lg font-bold text-center md:text-left">
+          Form Components
+        </h3>
+        <ul>
+          {fieldTypes.map(({ type, label, icon }) => (
+            <li
+              key={type}
+              className={`flex items-center gap-2 border p-2 my-2 cursor-pointer rounded-md hover:bg-[#d7e8ff] transition-all duration-200 ${
+                selectedField === type ? "bg-[#d7e8ff]" : ""
+              }`}
+              onClick={() => dispatch(selectField(type))}
+            >
+              <span
+                className={`p-2 text-lg rounded-sm ${
+                  selectedField === type
+                    ? "bg-white"
+                    : "bg-[#d7e8ff] hover:bg-white text-sky-800"
                 }`}
-                onClick={() => dispatch(selectField(type))}
               >
-                <span
-                  className={`p-2 text-lg rounded-sm ${
-                    selectedField === type
-                      ? "bg-white"
-                      : "bg-[#d7e8ff] hover:bg-white text-sky-800"
-                  }`}
-                >
-                  {icon}
-                </span>
-                {label}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex flex-col items-center gap-y-6">
-          <Link
-            to="/responses"
-            className="p-2 border rounded-sm border-gray-300 font-medium"
-          >
-            View Responses
-          </Link>
-          <Link
-            to="/"
-            className="p-1 flex rounded-sm justify-center items-center border border-gray-400"
-          >
-            <span className="text-xl">
-              <MdLogout />
-            </span>
-            LOGOUT
-          </Link>
-        </div>
+                {icon}
+              </span>
+              {label}
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+      <div className="flex flex-col items-center gap-y-6 w-full">
+        <Link
+          to="/responses"
+          className="p-2 border rounded-md border-gray-300 font-medium w-full text-center"
+        >
+          View Responses
+        </Link>
+        <Link
+          to="/"
+          className="p-1 flex rounded-md justify-center items-center border border-gray-400 w-full text-center"
+        >
+          <span className="text-xl">
+            <MdLogout />
+          </span>
+          <span className="ml-2">LOGOUT</span>
+        </Link>
+      </div>
+    </div>
   );
 }
 
